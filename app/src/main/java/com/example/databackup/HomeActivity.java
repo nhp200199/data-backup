814d1d1c456ddf6499db4a2ea1bc4cdcf05aff89
Phuc Nguyen <nhp200199@gmail.com>
2022-07-03 22:07:29 +0700

    log out user with menu option

--- app/src/main/java/com/example/databackup/HomeActivity.java
+++ app/src/main/java/com/example/databackup/HomeActivity.java
@@ -1,17 +1,30 @@
 package com.example.databackup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.databackup.databinding.ActivityHomeBinding;
 import com.example.databackup.databinding.ActivityMainBinding;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity {
     public static final String EXTRA_USER_EMAIL = "user_email";
     private String userEmail = "...";
     private ActivityHomeBinding binding;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +37,40 @@ public class HomeActivity extends AppCompatActivity {
             userEmail = receivedIntent.getStringExtra(EXTRA_USER_EMAIL);
         }
 
+        mAuth = FirebaseAuth.getInstance();
+
         binding.tvWelcomeUserEmail.setText(String.format(getString(R.string.home_activity_txt_welcome_user_with_argument), userEmail));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.home_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.opt_sign_out:
+                signOut();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
+//        mGoogleSignInClient.signOut()
+//                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        // ...
+//                    }
+//                });
     }
 }
